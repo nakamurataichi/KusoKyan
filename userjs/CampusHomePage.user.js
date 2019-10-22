767badec9aa9c75bb39275d6ce1460695666b8f0
taichi <nakamurataichi@users.noreply.github.com>
2019-10-22 19:41:15 +0900

    created userjs directory

---
+++ userjs/CampusHomePage.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name     CustomCampusHomePage
+// @version  1
+// @include  https://dh.force.com/digitalCampus/CampusHomePage
+// @grant    none
+// @run-at document-end
+// ==/UserScript==
+(() => {
+  // ここにサイト読み込み時，実行するJavaScriptを書く．
+
+})();
