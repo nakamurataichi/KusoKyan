7b34cbf8f5a180f26e27177e8757f41b0bcdb09b
taichi <nakamurataichi@users.noreply.github.com>
2019-10-22 22:58:41 +0900

    CSSを少し変えた

--- userjs/CampusHomePage.user.js
+++ userjs/CampusHomePage.user.js
@@ -1,11 +1,27 @@
 // ==UserScript==
 // @name     CustomCampusHomePage
 // @version  1
-// @include  https://dh.force.com/digitalCampus/CampusHomePage
+// @include  ^https://dh.force.com/digitalCampus/CampusHomePage$
 // @grant    none
 // @run-at document-end
 // ==/UserScript==
 (() => {
   // ここにサイト読み込み時，実行するJavaScriptを書く．
-
+  const customCSS = `
+  #wrapper {
+    width: auto;
+  }
+  #mainContents {
+    width: auto;
+    float: none;
+    display: grid;
+    grid-template-columns: repeat(2, 4fr);
+    grid-template-rows: repeat(2, 4fr);
+    grid-row-gap: 15px;
+    grid-column-gap: 15px;
+  }
+`;
+  const custumStyle = document.createElement('style');
+  custumStyle.textContent = customCSS;
+  document.body.appendChild(custumStyle);
 })();
